50e1d08d4b1288a142e495743d612cdcaba07ab8
WIN-JJH83PJTM7J\mkadmin <r.yakupov81@yandex.ru>
2022-11-24 17:50:16 +0300

    добавил небольшое меню

--- main_exchange.py
+++ main_exchange.py
@@ -1,22 +1,22 @@
-import data_request_exchange as dre
-import save_data_exchange as sde
-import data_viewer_output as dvo
-import data_processing_request as dpr
-import data_output_somewhere as dos
-from flask import Flask
+import data_controler as clic
+# import data_request_exchange as dre
+# import save_data_exchange as sde
+# import data_viewer_output as dvo
+# import data_processing_request as dpr
+# import data_output_somewhere as dos
+# from flask import Flask
 # import save_data_exchange_log as sdel
 
 
-# data_url = 'https://myfin.by/currency/cb-rf'
-# data_fr, = dre.get_data_request_exchange(data_url)
-data = dre.get_data_request_exchange_html()
-data1 = dre.get_data_request_exchange_api()
-print(data1)
-sde.get_save_data_exchange(data1)
-dvo.get_data_viewer_output(data1)
 
+# data = dre.get_data_request_exchange_html()
+# data1 = dre.get_data_request_exchange_api()
+# print(data1)
+# sde.get_save_data_exchange(data1)
+# dvo.get_data_viewer_output(data1)
 # test1 = [['доллар', 60.37, 'USD', 1], ['Евро', 62.44, 'EUR', 1], ['Казахстанский тенге', 13.1520, 'KZT', 100], ['Турецкая лира', 3.2437, 'TRY', 1], ['Узбекский сум', 53.8661, 'UZS',10000], ['Азербайджанский манат', 35.5142, 'AZN', 1]]
 # sde.get_save_data_exchange(test1)
 # dvo.get_data_viewer_output(test1)
-
 # sdel.get_save_data_exchange_log()
+
+clic.button_menu()
